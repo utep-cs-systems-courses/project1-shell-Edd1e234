cf78a905afa559576b0dcd03860bd7a05028d1e7
Eddie Garcia <edd1e234g@gmail.com>
2020-09-08 17:04:25 -0600

    Add pipes

--- shell/shell.py
+++ shell/shell.py
@@ -5,28 +5,28 @@ while (1):
         prompt_input = input(os.environ['PS1']) # Add input.
     else:
         prompt_input = input("$ ")
-    args = prompt_input.split(" ")
+    pipes = prompt_input.split(" | ")
+    for pipe in pipes:
+        args = pipe.split(" ")
+        if args[0] == "exit":
+            sys.exit(1)
 
-    if args[0] == "exit":
-        sys.exit(1)
-
-    rc = os.fork()
-
-    if rc < 0:
-        os.write(2,
-            ("fork failed, returning %d\n" % rc).encode())
-        sys.exit(1)
-    elif rc == 0:
-        process_flag = False
-        for dir in re.split(":", os.environ['PATH']):
-            program = "%s/%s" % (dir, args[0])
-            try:
-                process_flag = True
-                os.execve(program, args, os.environ)
-            except FileNotFoundError:
-                pass
-        if process_flag:
-            print("'" + args[0] +"' command not found.")
-        sys.exit(1)
-    else:
-        os.wait()
+        rc = os.fork()
+        if rc < 0:
+            os.write(2,
+                ("fork failed, returning %d\n" % rc).encode())
+            sys.exit(1)
+        elif rc == 0:
+            process_flag = False
+            for dir in re.split(":", os.environ['PATH']):
+                program = "%s/%s" % (dir, args[0])
+                try:
+                    process_flag = True
+                    os.execve(program, args, os.environ)
+                except FileNotFoundError:
+                    pass
+            if process_flag:
+                print("'" + args[0] +"' command not found.")
+            sys.exit(1)
+        else:
+            os.wait()
