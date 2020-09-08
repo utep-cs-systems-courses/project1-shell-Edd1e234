d76b8e2edcb6c4dadee5085284a4428a4c995ebb
Eddie Garcia <edd1e234g@gmail.com>
2020-09-08 16:07:16 -0600

    Fixed bugs, parent process waits for child to terminate

--- shell/shell.py
+++ shell/shell.py
@@ -5,16 +5,19 @@ while (1):
         prompt_input = input(os.environ['PS1']) # Add input.
     else:
         prompt_input = input("$ ")
+    args = prompt_input.split(" ")
+
+    print(args[0])
+    if args[0] == "exit":
+        sys.exit(1)
+
     rc = os.fork()
+
     if rc < 0:
-        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        os.write(2,
+            ("fork failed, returning %d\n" % rc).encode())
         sys.exit(1)
     elif rc == 0:
-        args = prompt_input.split()
-
-        # Quietly
-        if len(args) is 0:
-            sys.exit(1)
-
-        for dir in re.split(":", os.environ['PATH']):
-            program = "%s/%s" % (dir, args[0])
+        sys.exit(1)
+    else:
+        os.wait()
