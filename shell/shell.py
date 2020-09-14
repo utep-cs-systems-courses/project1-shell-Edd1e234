846c1a0171136d5d50f7f6e23a1f1176b1feb615
Eddie Garcia <edd1e234g@gmail.com>
2020-09-14 14:10:09 -0600

    Removed the for loop.

--- shell/shell.py
+++ shell/shell.py
@@ -6,27 +6,24 @@ while (1):
     else:
         prompt_input = input("$ ")
     pipes = prompt_input.split(" | ")
-    for pipe in pipes:
-        args = pipe.split(" ")
-        if args[0] == "exit":
-            sys.exit(1)
-
+    if args[0] == "exit":
+        sys.exit(1)
         rc = os.fork()
-        if rc < 0:
-            os.write(2,
+    if rc < 0:
+        os.write(2,
                 ("fork failed, returning %d\n" % rc).encode())
-            sys.exit(1)
-        elif rc == 0:
-            process_flag = False
-            for dir in re.split(":", os.environ['PATH']):
-                program = "%s/%s" % (dir, args[0])
-                try:
-                    process_flag = True
-                    os.execve(program, args, os.environ)
-                except FileNotFoundError:
-                    pass
-            if process_flag:
-                print("'" + args[0] +"' command not found.")
-            sys.exit(1)
-        else:
-            os.wait()
+        sys.exit(1)
+    elif rc == 0:
+        process_flag = False
+        for dir in re.split(":", os.environ['PATH']):
+            program = "%s/%s" % (dir, args[0])
+            try:
+                process_flag = True
+                os.execve(program, args, os.environ)
+            except FileNotFoundError:
+                pass
+        if process_flag:
+            print("'" + args[0] +"' command not found.")
+        sys.exit(1)
+    else:
+        os.wait()
