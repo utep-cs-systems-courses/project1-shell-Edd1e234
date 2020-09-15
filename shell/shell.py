d107dcbd15d9f4d2c9c68eb7dfc45b83d64119f4
Eddie Garcia <edd1e234g@gmail.com>
2020-09-15 14:55:31 -0600

    Parse file done.

--- shell/shell.py
+++ shell/shell.py
@@ -1,18 +1,50 @@
 import os, sys, time, re
 
+def parse_single_cmd(cmd):
+        """
+        Assumes command has already been split by '|'.
+        """
+        if cmd.lower() == "exit":
+            sys.exit(1)
+
+        if cmd[-1] == "\n":
+            cmd = cmd[0:-1]
+
+        from_file_name = to_file_name = None
+        cmd = " " + cmd + " "
+
+        # isolate < from_file_name
+        from_file_name = re.match("([^<]+)<[ ]*([^ ]*)(.*)", cmd)
+
+        if from_file_name:
+            pre, from_file_name, post = from_file_name.groups()
+            cmd = pre + " " + post
+
+        # isolate > to_file_name
+        has_to_file = re.match("([^>]+)>[ ]*([^ ]*)(.*)", cmd)
+        if has_to_file:
+            pre, to_file_name, post = has_to_file.groups()
+            cmd = pre + " " + post
+
+        cmd = re.sub(" +", " ", cmd) # remove extra spaces between tokens
+        cmd = cmd[1:-1]
+        return re.split(" +", cmd), from_file_name, has_to_file
+
 while (1):
     if os.environ.get('$PS1') != None:
         prompt_input = input(os.environ['$PS1']) # Add input.
-        print("here")
     else:
         prompt_input = input("$ ")
-    args = prompt_input.split(" | ")
+    args = prompt_input.split(" ")
 
     if args[0] == "":
         continue
 
-    if args[0] == "exit":
-        sys.exit(1)
+    args, from_file_name, has_to_file = parse_single_cmd(prompt_input)
+
+    # Need to sperate pipes here.
+
+    print(args)
 
     rc = os.fork()
     if rc < 0:
