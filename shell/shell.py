11e1fdada8c8e8811d71f8fa794ff5c0c505d72b
Eddie Garcia <edd1e234g@gmail.com>
2020-09-15 19:26:28 -0600

    Add Pipe implementation, refactored

--- shell/shell.py
+++ shell/shell.py
@@ -3,10 +3,11 @@ import os, sys, time, re
 def parse_single_cmd(cmd):
         """
         Assumes command has already been split by '|'.
-        """
-        if cmd.lower() == "exit":
-            sys.exit(1)
 
+        Returns list of items. First item is args for the command to execute,
+        the second and third are match objects for input and output file.
+        If present of course.
+        """
         if cmd[-1] == "\n":
             cmd = cmd[0:-1]
 
@@ -28,27 +29,57 @@ def parse_single_cmd(cmd):
 
         cmd = re.sub(" +", " ", cmd) # remove extra spaces between tokens
         cmd = cmd[1:-1]
-        return re.split(" +", cmd), from_file_name, has_to_file
+        return re.split(" +", cmd), has_to_file, from_file_name
+
+def excute_program(single_cmd):
+    """
+    Will excute a program. In process.
+    """
+    args, has_to_file, from_file_name = parse_single_cmd(single_cmd)
+    print("Args are: ")
+    print(args)
+    print(has_to_file)
+
+    process_flag = False
+    if has_to_file:
+        os.close(1)
+        os.open(has_to_file.groups()[1], os.O_CREAT | os.O_WRONLY)
+        os.set_inheritable(1, True)
+    if from_file_name:
+        args.append(from_file_name)
+
+    for dir in re.split(":", os.environ['PATH']):
+        program = "%s/%s" % (dir, args[0])
+        try:
+            process_flag = True
+            os.execve(program, args, os.environ)
+        except FileNotFoundError:
+            pass
+    if process_flag:
+        print("'" + args[0] +"' command not found.")
+    sys.exit(1)
 
 while (1):
     if os.environ.get('$PS1') != None:
         prompt_input = input(os.environ['$PS1']) # Add input.
     else:
         prompt_input = input("$ ")
-    args = prompt_input.split(" ")
 
-    if args[0] == "":
+    if prompt_input == "":
         continue
 
-    args, from_file_name, has_to_file = parse_single_cmd(prompt_input)
+    if prompt_input.lower() in "exit":
+        sys.exit(1)
 
     # change directory
-    if "cd" in args:
-        process_flag = True;
-        os.chdir(os.getcwd() + "/" + args[1])
+    if "cd" in prompt_input:
+        args = prompt_input.split(" ")
+        try:
+            process_flag = True;
+            os.chdir(os.getcwd() + "/" + args[1])
+        except FileNotFoundError:
+            print("Does not exist.")
         continue
-
-    # Need to sperate pipes here.
 
     rc = os.fork()
     if rc < 0:
@@ -56,24 +87,36 @@ while (1):
                 ("fork failed, returning %d\n" % rc).encode())
         sys.exit(1)
     elif rc == 0:
-        process_flag = False
-        if has_to_file:
-            os.close(1)
-            os.open(has_to_file.groups()[1], os.O_CREAT | os.O_WRONLY)
-            os.set_inheritable(1, True)
+        if "|" in prompt_input:
+            print("Pipe found.")
+            pipe = prompt_input.split("|")
+            cmd_1 = pipe[0].split()
+            cmd_2 = pipe[1].split()
 
-        if from_file_name:
-            args.append(from_file_name)
+            pr, pw = os.pipe() # file descriptors for reading and writing.
+            for file_descriptor in (pr, pw):
+                os.set_inheritable(file_descriptor, True)
 
-        for dir in re.split(":", os.environ['PATH']):
-            program = "%s/%s" % (dir, args[0])
-            try:
-                process_flag = True
-                os.execve(program, args, os.environ)
-            except FileNotFoundError:
-                pass
-        if process_flag:
-            print("'" + args[0] +"' command not found.")
-        sys.exit(1)
+            pipe_rc = os.fork()
+            if pipe_rc < 0:
+                os.write(2, ("fork faile").encode())
+                sys.exit(1)
+
+            elif pipe_rc == 0:
+                os.close(1)
+                os.dup(pw)
+                os.set_inheritable(1, True)
+                for file_descriptor in (pr, pw):
+                    os.close(fd)
+                excute_program(cmd_1)
+            else:
+                os.close(0)
+                os.dup(pr)
+                os.set_inheritable(0, True)
+                for file_descriptor in (pw, pr):
+                    os.close(file_descriptor)
+                excute_program(cmd_2)
+        else:
+            excute_program(prompt_input)
     else:
         os.wait()
