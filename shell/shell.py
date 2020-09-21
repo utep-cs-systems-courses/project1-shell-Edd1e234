394f78a79919f0c01673807ba2b432cca6fc841b
Eddie Garcia <edd1e234g@gmail.com>
2020-09-20 21:20:05 -0600

    Cleanup, testShell still no good

--- shell/shell.py
+++ shell/shell.py
@@ -50,8 +50,42 @@ def excute_program(args):
         # os.write(1, ("##########").encode())
     sys.exit(1)
 
-def run_command(prompt_input):
+def run_pipe(cmd):
+    cmd_1 = cmd[:cmd.index('|')]
+    cmd_2 = cmd[cmd.index('|')+1:]
 
+    if DEBUG:
+        print("cmd_1", cmd_1)
+        print("cmd_2", cmd_2)
+
+    pr, pw = os.pipe() # file descriptors for reading and writing.
+    for file_descriptor in (pr, pw):
+        os.set_inheritable(file_descriptor, True)
+
+    pipe_rc = os.fork()
+    if pipe_rc < 0:
+        os.write(2, ("fork FAILED").encode())
+        sys.exit(1)
+
+    elif pipe_rc == 0:
+        os.close(1)
+        os.dup(pw)
+        os.set_inheritable(1, True)
+        for file_descriptor in (pr, pw):
+            os.close(file_descriptor)
+        excute_program(cmd_1)
+    else:
+        os.close(0) # Close the input.
+        os.dup(pr)
+        os.set_inheritable(0, True)
+        for file_descriptor in (pw, pr):
+            os.close(file_descriptor)
+        if "|" in cmd_2:
+            run_pipe(cmd_2)
+        excute_program(cmd_2)
+
+
+def run_command(prompt_input):
         rc = os.fork()
         if rc < 0:
             os.write(2, ("fork failed, returning %d\n" % rc).encode())
@@ -59,41 +93,18 @@ def run_command(prompt_input):
         elif rc == 0:
             if DEBUG:
                 os.write(1,"inside rc\n".encode())
-
             if "|" in prompt_input:
-                cmd_1 = prompt_input[:prompt_input.index('|')]
-                cmd_2 = prompt_input[prompt_input.index('|')+1:]
-
-                pr, pw = os.pipe() # file descriptors for reading and writing.
-                for file_descriptor in (pr, pw):
-                    os.set_inheritable(file_descriptor, True)
-
-                pipe_rc = os.fork()
-                if pipe_rc < 0:
-                    os.write(2, ("fork FAILED").encode())
-                    sys.exit(1)
-
-                elif pipe_rc == 0:
-                    os.close(1)
-                    os.dup(pw)
-                    os.set_inheritable(1, True)
-                    for file_descriptor in (pr, pw):
-                        os.close(file_descriptor)
-                    excute_program(cmd_1)
-                else:
-                    os.close(0)
-                    os.dup(pr)
-                    os.set_inheritable(0, True)
-                    for file_descriptor in (pw, pr):
-                        os.close(file_descriptor)
-                    excute_program(cmd_2)
+                if DEBUG:
+                    os.write(1, ("pipe\n").encode())
+                run_pipe(prompt_input)
             else:
                 if DEBUG:
                     os.write(1,"EXCUTE PROGRAM".encode())
                     os.write(1, (str(prompt_input) + "\n").encode())
                 excute_program(prompt_input)
         else:
-            os.wait()
+            if prompt_input[-1] != "&":
+                os.wait()
 
 
 while (1):
@@ -103,40 +114,34 @@ while (1):
     os.write(1, prompt.encode())
     try:
         prompt_input = os.read(0, AMOUNT_OF_CHAR)
-        if len(prompt_input) == 1:
-            continue
+        if len(prompt_input) == 0:break
+        if len(prompt_input) == 1:continue
         prompt_input = prompt_input.decode().split("\n")
         if DEBUG:
             os.write(1,("Right After prompt\n").encode())
             os.write(1, (str(prompt_input) + "\n").encode())
     except EOFError:
         sys.exit(1)
+    prompt_input.remove("")
 
     if "exit" in prompt_input:
         sys.exit(1)
 
-    prompt_input[0] = prompt_input[0].split()
-
-    if DEBUG:
-        os.write(1, ("Moving into cd..").encode())
-        os.write(1, (str(prompt_input) + "\n").encode())
-
     # change directory
     if "cd" in prompt_input[0]:
-        args = prompt_input[0]
+        args = prompt_input[0].split()
         try:
             if DEBUG:
                 os.write(1, ("Changing Directory\n" + args[1] + "\n").encode())
             os.chdir(args[1])
         except FileNotFoundError:
-            os.write(2, ("cd FAILED").encode())
+            os.write(2, (args[1] + " does not exist").encode())
         continue
 
-    if DEBUG:
-        os.write(1, (str(prompt_input) + "\n").encode())
+    #if DEBUG:
+    #    os.write(1, (str(prompt_input) + "\n").encode())
     for arg in prompt_input:
-        if "" in arg:
-            continue
+        arg = arg.split()
         if DEBUG:
             os.write(1, ("--------------").encode())
             os.write(1, ("AFTER COMBINE").encode())
